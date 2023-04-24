7c2de4099c0d21990a794962b23983b0180ebe80
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-24 22:48:47 +0600

    template, urls, views added

--- payapp/views.py
+++ payapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+
+
+def index(request, id):
+    return HttpResponse(render(request, 'payapp/index.html', {'id': id}))
