8c4f2da17839f237735f962679cdaa1b0fa97f79
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-25 17:44:14 +0600

    Navbar, logo, favicon, meta added

--- payapp/views.py
+++ payapp/views.py
@@ -7,6 +7,6 @@ import requests
 # Create your views here.
 
 
-def index(request, id):
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/{id}")
+def index(request):
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/5")
     return HttpResponse(render(request, 'payapp/index.html', {'res': json.loads(response.content)}))
