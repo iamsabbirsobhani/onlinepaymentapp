29e2a717b660a4e20deef6ec0e38727bb7eac63f
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-25 08:21:00 +0600

    rename the db accordingly, and refactored

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'webapps2023.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.webapps',
     }
 }
 
