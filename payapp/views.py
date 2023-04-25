53aa86458202c818920863370174afdce547770a
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-25 14:35:29 +0600

    tailwindcss added

--- payapp/views.py
+++ payapp/views.py
@@ -1,8 +1,12 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 # Create your views here.
 
 
 def index(request, id):
-    return HttpResponse(render(request, 'payapp/index.html', {'id': id}))
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/{id}")
+    return HttpResponse(render(request, 'payapp/index.html', {'res': json.loads(response.content)}))
