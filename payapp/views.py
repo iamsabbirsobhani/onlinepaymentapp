461c2ac3cd26459a93a6a7981b001dad5689faa0
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-25 19:36:56 +0600

    HomePage added plus Navbar, logo, favicon, meta added

--- payapp/views.py
+++ payapp/views.py
@@ -8,5 +8,4 @@ import requests
 
 
 def index(request):
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/5")
-    return HttpResponse(render(request, 'payapp/index.html', {'res': json.loads(response.content)}))
+    return HttpResponse(render(request, 'payapp/homePage.html'))
