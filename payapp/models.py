b523c576fe641b34f84a2771b62e249957c5bbcb
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-27 10:32:17 +0600

    dj: User model added

--- payapp/models.py
+++ payapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, default="")
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    balance = models.IntegerField(default=0)
+    currency_type = models.CharField(max_length=50, default="GBP")
+
+    def __str__(self):
+        return self.username
+
