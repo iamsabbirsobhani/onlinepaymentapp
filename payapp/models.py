37292267fb655af02d23937bdae089f9f881d4c0
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2023-04-27 14:03:14 +0600

    dj: CurrencyType model added

--- payapp/models.py
+++ payapp/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 
 class User(models.Model):
+    currency_type = models.ForeignKey("CurrencyType", on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, default="")
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     balance = models.IntegerField(default=0)
-    currency_type = models.CharField(max_length=50, default="GBP")
 
     def __str__(self):
         return self.username
 
+
+class CurrencyType(models.Model):
+    currency_type = models.CharField(max_length=50, default="GBP")
+
+    def __str__(self):
+        return self.currency_type
